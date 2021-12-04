b0c45f676bc5d063a2aca2e3d05ff6c4f39092d7
Trần Cao Minh Bách <45133811+bachtran2000@users.noreply.github.com>
2021-12-04 17:28:25 +0700

    Add files via upload

--- t2sUI.py
+++ t2sUI.py
@@ -233,7 +233,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Text to Speech Bart Tran"))
         self.comboBox.setItemText(0, _translate("MainWindow", "Chọn..."))
         self.comboBox.setItemText(1, _translate("MainWindow", "Nữ - Miền Nam"))
         self.comboBox.setItemText(
